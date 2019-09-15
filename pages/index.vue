6d0bfac6e92cd6c2006a32b88bbcd7d83491522e
siijima0113 <everyth1ng.n1ce.St01@gmail.com>
2019-09-15 20:11:14 +0900

    1番目のコミット

---
+++ pages/index.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="othello-box">
+    <template v-for="y in board.length">
+      <div
+        v-for="x in board[y - 1].length"
+        :key="`${x}-${y}`"
+        class="othello-coin"
+        @click="checkPutable(x, y)"
+      >
+        <div
+          v-if="board[y - 1][x - 1] !== 0"
+          :class="['stone', board[y - 1][x - 1] === 1 ? 'white' : 'black']"
+        />
+      </div>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      board: [
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, -1, 1, 0, 0, 0],
+        [0, 0, 0, 1, -1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0]
+      ]
+    }
+  },
+  methods: {
+    checkPutable (x, y) {
+      let putable = false
+      const board = this.board
+
+      if (board[y - 1][x - 1] === 0) {
+        putable = true
+      } else {
+        alert('ここにはおけません！')
+      }
+      console.log(x, y, putable)
+    }
+  }
+}
+</script>
+
+<style>
+.othello-box{
+  height: 640px;
+  width: 640px;
+  background-color: green;
+  margin: 50px auto;
+}
+
+.othello-coin{
+  height:12.5%;
+  width: 12.5%;
+  border: solid 1px black;
+  box-sizing: border-box;
+  float:left;
+}
+
+.stone{
+  width: 80%;
+  height: 80%;
+  margin: 10%;
+  border-radius: 50%;
+}
+
+.black{
+  background: black;
+}
+
+.white{
+  background: white;
+}
+</style>
